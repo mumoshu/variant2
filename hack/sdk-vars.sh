a7256d54521b492120f270d9fb36020515c1ca36
Yusuke Kuoka <ykuoka@gmail.com>
2020-12-06 16:22:17 +0900

    Fix smoke and release jobs

--- hack/sdk-vars.sh
+++ hack/sdk-vars.sh
@@ -11,3 +11,8 @@ fi
 
 export VERSION=${tag}
 export MOD_REPLACES=$(go run hack/print-replaces.go)
+
+if [ ! -z "${GITHUB_ENV}" ]; then
+  echo "VERSION=${VERSION}" >> $GITHUB_ENV
+  echo "MOD_REPLACES=${MOD_REPLACES}" >> $GITHUB_ENV
+fi
